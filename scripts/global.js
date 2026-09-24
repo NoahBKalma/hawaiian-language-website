7b379fb76d66e16201a27f44119f75c4035aefa2
Noah Kalma <nkalma0001@gmail.com>
2026-09-24 00:11:33 -0400

    added about us bar main page

--- scripts/global.js
+++ scripts/global.js
@@ -1,7 +1,5 @@
-import { isLoggedIn, authFetch } from '/scripts/auth.js'
+import { authFetch } from '/scripts/auth.js'
 import { API_BASE_URL } from "/scripts/config.js";
-
-const headerLoginRegisterButton = document.getElementById(`page-login-button`);
 
 export async function getLoggedInUsername() {
     const response = await authFetch(`${API_BASE_URL}/signed-in-user`);
@@ -10,3 +8,5 @@ export async function getLoggedInUsername() {
         return data.username;
     }
 }
+
+document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + 'px');
